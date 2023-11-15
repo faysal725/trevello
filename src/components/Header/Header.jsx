992837c1e6246ed81ce1f65d0e7217d374bf35e4
NedBud <nedbud@pop-os.localdomain>
2023-11-15 16:41:50 +0600

    navbar is builded

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ContactBar from '../../shared/ContactBar'
+import NavBar from '../../shared/NavBar'
 
 export default function Header() {
   return (
     <div className=''>
         <ContactBar/>
+        <NavBar />
     </div>
   )
 }
