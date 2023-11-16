ad2c5a6fd8cce899550f6e4665b30af6edbfc312
faysal725 <afaysal725@gmail.com>
2023-11-16 10:56:03 +0600

    footer completed

--- src/components/Layout/User.jsx
+++ src/components/Layout/User.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 export default function User() {
   return (
     <div>
         <Header />
-        <Outlet />
+        <div className='min-h-screen'>
+         <Outlet />
+        </div>
+
+        <Footer />
     </div>
   )
 }
--- src/shared/Logo.jsx
+++ src/shared/Logo.jsx
@@ -1,11 +1,11 @@
 import { PaperAirplaneIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 
-export default function Logo() {
+export default function Logo({textCol = "text-black"}) {
   return (
-    <div className='mx-auto md:mx-0 flex items-center'>
+    <div className={'mx-auto md:mx-0 flex items-center '+textCol}>
         <PaperAirplaneIcon className='h-5 w-5 text-blue-800' />
-        <p>Travello</p>
+        <p className='font-bold italic'>Travello</p>
     </div>
   )
 }
